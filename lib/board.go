243ed7a1dc3723790746a1025c5d845c328382ce
Dave Jaccard <dave.jaccard@gmail.com>
2018-08-24 11:57:19 -0600

    added trace rewrote String() and Base3() removed comments

--- lib/board.go
+++ lib/board.go
@@ -36,44 +36,48 @@ func NewBoard() Board {
 	for i := byte(0); i < byte(numSquares); i++ {
 		b.board[i].state = 0
 	}
+	Trace.Println("Created new board.")
 	return b
 }
 
+// String returns the board in a visual tic tac toe paradigm
 func (b Board) String() string {
+	Trace.Println("Call to String()")
 	var output bytes.Buffer
-	output.WriteString(b.board[0].String())
-	output.WriteString("|")
-	output.WriteString(b.board[1].String())
-	output.WriteString("|")
-	output.WriteString(b.board[2].String())
-	output.WriteString("\n-+-+-\n")
-	output.WriteString(b.board[3].String())
-	output.WriteString("|")
-	output.WriteString(b.board[4].String())
-	output.WriteString("|")
-	output.WriteString(b.board[5].String())
-	output.WriteString("\n-+-+-\n")
-	output.WriteString(b.board[6].String())
-	output.WriteString("|")
-	output.WriteString(b.board[7].String())
-	output.WriteString("|")
-	output.WriteString(b.board[8].String())
-	output.WriteString("\n")
+	output.WriteString(
+		fmt.Sprintf(
+			"%v|%v|%v\n-+-+-\n%v|%v|%v\n-+-+-\n%v|%v|%v\n",
+			b.board[0].String(),
+			b.board[1].String(),
+			b.board[2].String(),
+			b.board[3].String(),
+			b.board[4].String(),
+			b.board[5].String(),
+			b.board[6].String(),
+			b.board[7].String(),
+			b.board[8].String()))
 	return output.String()
 }
 
 // Base3 returns the base 3 string for the board state
 func (b Board) Base3() string {
 	var output bytes.Buffer
-	output.WriteString(strconv.FormatInt(int64(b.board[0].Byte()), 10))
-	output.WriteString(strconv.FormatInt(int64(b.board[1].Byte()), 10))
-	output.WriteString(strconv.FormatInt(int64(b.board[2].Byte()), 10))
-	output.WriteString(strconv.FormatInt(int64(b.board[3].Byte()), 10))
-	output.WriteString(strconv.FormatInt(int64(b.board[4].Byte()), 10))
-	output.WriteString(strconv.FormatInt(int64(b.board[5].Byte()), 10))
-	output.WriteString(strconv.FormatInt(int64(b.board[6].Byte()), 10))
-	output.WriteString(strconv.FormatInt(int64(b.board[7].Byte()), 10))
-	output.WriteString(strconv.FormatInt(int64(b.board[8].Byte()), 10))
+	for _, x := range b.board {
+		output.WriteString(
+			//fmt.Sprintf("%d", strconv.FormatInt(int64(x.Byte()), 10)))
+			fmt.Sprintf("%d", x.Byte()))
+	}
+	/*
+		strconv.FormatInt(int64(b.board[0].Byte()), 10),
+		strconv.FormatInt(int64(b.board[1].Byte()), 10),
+		strconv.FormatInt(int64(b.board[2].Byte()), 10),
+		strconv.FormatInt(int64(b.board[3].Byte()), 10),
+		strconv.FormatInt(int64(b.board[4].Byte()), 10),
+		strconv.FormatInt(int64(b.board[5].Byte()), 10),
+		strconv.FormatInt(int64(b.board[6].Byte()), 10),
+		strconv.FormatInt(int64(b.board[7].Byte()), 10),
+		strconv.FormatInt(int64(b.board[8].Byte()), 10))
+	*/
 	return fmt.Sprintf("%09s", output.String())
 }
 
@@ -100,14 +104,6 @@ func (b *Board) SetFromInt(newValue uint64) (err error) {
 func (b *Board) SetFromBase3(newValue string) (err error) {
 	err = nil
 	strRepresentation := fmt.Sprintf("%09s", newValue)
-	/*
-		ul, err := strconv.ParseInt(strRepresentation[0:1], 10, 8)
-		if err != nil {
-			Error.Printf("%v: %v: %v", err, newValue)
-			return err
-		}
-	*/
-
 	for i := 0; i < 9; i++ {
 		x, err := strconv.ParseInt(strRepresentation[i:i+1], 10, 8)
 		if err != nil {
@@ -115,27 +111,6 @@ func (b *Board) SetFromBase3(newValue string) (err error) {
 		}
 		b.board[i].SetFromByte(byte(x))
 	}
-
-	/*
-		ul, _ := strconv.ParseInt(strRepresentation[0:1], 10, 8)
-		uc, _ := strconv.ParseInt(strRepresentation[1:2], 10, 8)
-		ur, _ := strconv.ParseInt(strRepresentation[2:3], 10, 8)
-		ml, _ := strconv.ParseInt(strRepresentation[3:4], 10, 8)
-		mc, _ := strconv.ParseInt(strRepresentation[4:5], 10, 8)
-		mr, _ := strconv.ParseInt(strRepresentation[5:6], 10, 8)
-		ll, _ := strconv.ParseInt(strRepresentation[6:7], 10, 8)
-		lc, _ := strconv.ParseInt(strRepresentation[7:8], 10, 8)
-		lr, _ := strconv.ParseInt(strRepresentation[8:9], 10, 8)
-		b.board[0].SetFromByte(byte(ul))
-		b.board[1].SetFromByte(byte(uc))
-		b.board[2].SetFromByte(byte(ur))
-		b.board[3].SetFromByte(byte(ml))
-		b.board[4].SetFromByte(byte(mc))
-		b.board[5].SetFromByte(byte(mr))
-		b.board[6].SetFromByte(byte(ll))
-		b.board[7].SetFromByte(byte(lc))
-		b.board[8].SetFromByte(byte(lr))
-	*/
 	return
 }
 
@@ -162,27 +137,26 @@ func (b *Board) Winner() (win bool, winner byte) {
 	win = false
 	winner = 0
 	for i := range winCombos {
-		//fmt.Println("testing ", winCombos[i])
+		// If any of the squares is empty, they don't count
 		if b.board[winCombos[i][0]].Byte() == 0 {
-			//fmt.Println(winCombos[i][0], " is 0, continue")
 			continue
 		}
 		if b.board[winCombos[i][1]].Byte() == 0 {
-			//fmt.Println(winCombos[i][0], " is 0, continue")
 			continue
 		}
 		if b.board[winCombos[i][2]].Byte() == 0 {
-			//fmt.Println(winCombos[i][0], " is 0, continue")
 			continue
 		}
+
+		// Now see if all the values are equal
 		if (b.board[winCombos[i][0]].Byte() == b.board[winCombos[i][1]].Byte()) &&
 			(b.board[winCombos[i][0]].Byte() == b.board[winCombos[i][2]].Byte()) {
 			winner = b.board[winCombos[i][0]].Byte()
 			win = true
-			//fmt.Println("winner: ", winner)
+			Trace.Printf("%v wins\n", winner)
 			return
 		}
-		//fmt.Println("no winner, continue")
 	}
+	Trace.Println("no winner, continue")
 	return
 }
